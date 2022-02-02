faac79159d1522518e1e44d82a71b7a364f635ed
Jawad Chowdhury <jawadc13@gmail.com>
2022-02-02 03:52:20 -0600

    Committing for Java Program

---
+++ basicjavaprogram/src/basicjavaprogram/helloworld.java
@@ -0,0 +1,10 @@
+package basicjavaprogram;
+
+public class helloworld {
+	
+	public static void main( String[] args )
+    {
+		System.out.println("Hello this is a basic program and first step to programming for HCL! Hello World!");
+    }
+	
+}
